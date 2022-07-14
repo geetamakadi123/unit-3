85ab21ff08461eb0428ca87234a88c839e639697
geeta <ggmakadi1997@gmail.com>
2022-07-14 09:52:07 -0400

    food

---
+++ food app/components/navbar.js
@@ -0,0 +1,30 @@
+function navbar(){
+    return `<h3>
+    <a href="/">Home</a>
+</h3>
+<h3>
+
+<a href="search.html">search</a>
+
+</h3>
+<div id="option">
+
+
+<h3>
+    <a href="random receipes.html">random receipes</a>
+</h3>
+
+
+<h3>
+    <a href="receipe of the day.html">receipe of the day</a>
+</h3>
+<h3>
+    <a href="login.html">login</a>
+</h3>
+<h3>
+    <a href="signup.html">signup</a>
+</h3>
+</div>`
+}
+
+export default navbar;
